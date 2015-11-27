fd9c1552f59737eb7e010da33a03f738da635493
joserobjr <jose.rob.jr@gmail.com>
2015-11-27 00:45:49 -0300

    Added support for Universal Coins

--- src/main/java/mytown/proxies/EconomyProxy.java
+++ src/main/java/mytown/proxies/EconomyProxy.java
@@ -25,6 +25,8 @@ public class EconomyProxy {
             return false;
         } else if(Config.instance.costItemName.get().equals(Economy.CURRENCY_VAULT)) {
             return false;
+        } else if(Config.instance.costItemName.get().equals(Economy.CURRENCY_UNIVERSAL_COINS)) {
+            return false;
         }
         return true;
     }
