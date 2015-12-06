58f866b33c240d41e27b7cad318522b8bdc9ae84
joserobjr <jose.rob.jr@gmail.com>
2015-12-06 05:56:37 -0300

    Added recept for sell transactions

--- src/main/java/mytown/entities/signs/SellSign.java
+++ src/main/java/mytown/entities/signs/SellSign.java
@@ -12,11 +12,17 @@ import mytown.entities.Resident;
 import mytown.new_datasource.MyTownUniverse;
 import mytown.proxies.EconomyProxy;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.tileentity.TileEntitySign;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -75,9 +81,11 @@ public class SellSign extends Sign {
 
         if (EconomyProxy.getEconomy().takeMoneyFromPlayer(resident.getPlayer(), price)) {
             boolean mayorOwner = true;
+            ArrayList<String> ownersName = new ArrayList<String>(plot.ownersContainer.size());
             for (Resident resInPlot : plot.ownersContainer) {
                 if(mayorOwner && !resInPlot.getUUID().equals(plot.getTown().residentsMap.getMayor().getUUID()))
                     mayorOwner = false;
+                ownersName.add(resInPlot.getPlayerName());
             }
 
             Resident.Container residentsToRemove = new Resident.Container();
@@ -97,6 +105,8 @@ public class SellSign extends Sign {
             MyTown.instance.datasource.linkResidentToPlot(resident, plot, true);
             resident.sendMessage(getLocal().getLocalization("mytown.notification.plot.buy.newOwner", plot.getName()));
             boolean payed = false;
+            double split = 0, change = 0;
+            int value = 0;
             if(!(mayorOwner || EconomyProxy.isItemEconomy() || owners.isEmpty()))
             {
                 if(owners.size() == 1) {
@@ -108,9 +118,9 @@ public class SellSign extends Sign {
                     }
                 }
                 else {
-                    double split = ((double)price) / owners.size();
-                    double change = 0;
-                    int value = (int) split;
+                    split = ((double)price) / owners.size();
+                    change = 0;
+                    value = (int) split;
                     List<IEconManager> ownerAccounts = new ArrayList<IEconManager>(owners.size());
                     for(Resident owner: owners) {
                         IEconManager eco = EconomyProxy.getEconomy().economyManagerForUUID(owner.getUUID());
@@ -142,6 +152,7 @@ public class SellSign extends Sign {
                 }
             }
             if(!payed) {
+                mayorOwner = true;
                 plot.getTown().bank.addAmount(price);
                 for(Resident owner: owners) {
                     owner.sendMessage(getLocal().getLocalization("mytown.notification.plot.buy.oldOwner", plot.getName(), EconomyProxy.getCurrency(price)));
@@ -150,6 +161,54 @@ public class SellSign extends Sign {
 
             deleteSignBlock();
             plot.deleteSignBlocks(signType, player.worldObj);
+
+            ItemStack stack = new ItemStack(Items.written_book);
+            stack.setTagInfo("author", new NBTTagString("#Recibo"));
+            stack.setTagInfo("title", new NBTTagString("Comprovante de compra de lote"));
+            NBTTagList pages = new NBTTagList();
+            Date dateObj = new Date();
+            String date = DateFormat.getDateInstance(DateFormat.SHORT).format(dateObj);
+            String time = DateFormat.getTimeInstance(DateFormat.LONG).format(dateObj);
+            String price = EconomyProxy.getCurrency(this.price);
+            pages.appendTag(new NBTTagString("=== IMPORTANTE ===\n" +
+                    "Guarde esse documento em local seguro, " +
+                    "a perda deste documento resultara na perda da garantia de seu lote.\n\n" +
+                    "Comprovante de aquisicao do lote "+plot.getName()+" da cidade "+plot.getTown().getName()+" por "+player.getCommandSenderName()+" no valor de "+price+" em "+date
+            ));
+            pages.appendTag(new NBTTagString("====== LOTE ======\n" +
+                    "Nome: "+plot.getName()+"\n" +
+                    "Cidade: "+plot.getTown().getName()+"\n" +
+                    "Dimensao: "+plot.getDim()+"\n"+
+                    "Localizacao: \n"+
+                    String.format("%-7d %-3d %-7d\n", plot.getStartX(), plot.getStartY(),plot.getStartZ()) +
+                    String.format("%-7d %-3d %-7d\n", plot.getEndX(), plot.getEndY(), plot.getEndZ()) +
+                    "Valor: "+price+"\n" +
+                    "Data: "+date+"\n"+
+                    "Hora: "+time+"\n"
+            ));
+            pages.appendTag(new NBTTagString("==== COMPRADOR ====\n" +
+                    "Nome: "+player.getCommandSenderName()+"\n" +
+                    "UUID: "+player.getPersistentID()+"\n" +
+                    "Data: "+date+"\n" +
+                    "Hora: "+time+"\n"));
+            pages.appendTag(new NBTTagString("===== VENDEDOR =====\n" +
+                    "Nome: "+owner.getPlayerName()+"\n" +
+                    "UUID: "+owner.getUUID()+"" +
+                    "Data: "+date+"\n" +
+                    "Hora: "+time+"\n"));
+            pages.appendTag(new NBTTagString("===== PAGAMENTO =====\n" +
+                    (mayorOwner? "Banco da prefeitura\nValor: "+price :
+                        "Dividido entre os donos\nValor: "+price+"\nDonos: "+ownersName.size()+"\nDivisao: "+split+"" +
+                            "\nTaxa: "+change+"\nRecebido: "+value)
+            ));
+            String nameString = ownersName.toString();
+            pages.appendTag(new NBTTagString("== ANTIGOS DONOS ==\n" +
+                    "Data: "+date+"\n" +
+                    "Hora: "+time+"\n" +
+                    "Nomes: "+ nameString.substring(1, nameString.length()-1)));
+            stack.setTagInfo("pages", pages);
+            player.inventory.addItemStackToInventory(stack);
+            player.inventoryContainer.detectAndSendChanges();
         } else {
             resident.sendMessage(getLocal().getLocalization("mytown.notification.plot.buy.failed", EconomyProxy.getCurrency(price)));
         }
